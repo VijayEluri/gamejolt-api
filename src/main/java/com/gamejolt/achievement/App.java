f08d1ff79be73ba2ce5e8f8d0dce132fc24512ef
born2snipe <born2snipe@gmail.com>
2010-02-06 10:15:25 -0600

    initial

---
+++ src/main/java/com/gamejolt/achievement/App.java
@@ -0,0 +1,13 @@
+package com.gamejolt.achievement;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
